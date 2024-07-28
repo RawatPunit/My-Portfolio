095a5ecc7d02bebee50277f37e9b7470d055fd77
RawatPunit <rawatpunit928@gmail.com>
2024-07-28 15:48:27 +0530

    project cards doen and abou to start SKILL Section

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import viberr from '../../assets/viberr.png';
+import freshVurger from "../../assets/fresh-burger.png"
 import styles from './ProjectsStyles.module.css';
+import ProjectCard from '../../common/ProjectCard';
+
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className='sectionTitle'>Projects</h1>
       <div className= {styles.projectContainer}>
-         <a 
-            href=""
-            target='_blank'>
-            <img className='hover' src={viberr} alt="projectname"/>
-            <h3>Project Name</h3>
-            <p>Yet to be confirmed</p>
-             
-         </a>
+        <ProjectCard src={viberr} 
+          link="{Link of the project }"
+          h3 = "Project Title Name"
+          p="project desc title"
+        />
+        <ProjectCard src={freshVurger} 
+          link="{Link of the project }"
+          h3 = "Project Title Name"
+          p="project desc title"
+        />
+        <ProjectCard src={viberr} 
+          link="{Link of the project }"
+          h3 = "Project Title Name"
+          p="project desc title"
+        />
       </div>
     </section>
   )
